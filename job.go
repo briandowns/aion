2fb5136c9ded10ac56e141c27ef3c970dab3e995
Brian Downs <brian.downs@gmail.com>
2016-01-03 08:52:28 -0700

    test files

--- job.go
+++ job.go
@@ -1,21 +1,49 @@
 package main
 
+import (
+	"sync"
+)
+
 // Jobber
 type Jobber interface {
-	Start() error
-	Stop() error
-	Status() *JobStatus
+	Enable()
+	Disable()
 }
 
 // JobStatus
 type JobStatus struct {
 	Job
-	Status string
+	Status bool
+	sync.Mutex
 }
 
 // NewJob creates a new reference to Job
 func (j *Job) NewJob(name string) *Job {
 	return &Job{
 		Name: name,
+	}
+}
+
+// Enable enables an unactive job
+func (j *JobStatus) Enable() {
+	switch j.Status {
+	case true:
+		return
+	case false:
+		j.Lock()
+		defer j.Unlock()
+		j.Status = true
+	}
+}
+
+// Disable disables an active job
+func (j *JobStatus) Disable() {
+	switch j.Status {
+	case false:
+		return
+	case true:
+		j.Lock()
+		defer j.Unlock()
+		j.Status = false
 	}
 }
