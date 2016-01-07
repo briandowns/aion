791d42474be9c65fd4a5d4af88c5f5223acd4c7a
Brian Downs <brian.downs@gmail.com>
2016-01-07 09:49:33 -0700

    frontend stuff

--- public/js/aion.js
+++ public/js/aion.js
@@ -2,6 +2,7 @@ var BASE_API = 'http://localhost:9898/api/v1/'
 
 var app = angular.module('aionApp', []);
 
+
 app.controller('mainController', function($scope, $http) {
 	$http.get(BASE_API + 'job').then(function(response) {$scope.jobs = response.data.jobs});
 	$http.get(BASE_API + 'task').then(function(response) {$scope.tasks = response.data.tasks});
