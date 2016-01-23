5fc6f8e6ba2467f25914a5f5ea1b77746c17964f
Brian Downs <brian.downs@gmail.com>
2016-01-23 13:00:35 -0700

    unexpected base and sub package refactor

---
+++ job_status.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"sync"
+
+	"github.com/briandowns/aion/database"
+)
+
+// JobStatuser
+type JobStatuser interface {
+	Enable()
+	Disable()
+}
+
+// JobStatus holds the
+type JobStatus struct {
+	database.Job
+	Status bool
+	sync.Mutex
+}
+
+// Enable enables an unactive job
+func (j *JobStatus) Enable() {
+	switch j.Status {
+	case true:
+		return
+	case false:
+		j.Lock()
+		defer j.Unlock()
+		j.Status = true
+	}
+}
+
+// Disable disables an active job
+func (j *JobStatus) Disable() {
+	switch j.Status {
+	case false:
+		return
+	case true:
+		j.Lock()
+		defer j.Unlock()
+		j.Status = false
+	}
+}
