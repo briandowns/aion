4593e67ef9cf6bcdd06109029c9b9d074f8f25d6
Brian Downs <brian.downs@gmail.com>
2016-01-24 21:53:31 -0700

    small changes

--- controllers/authentication.go
+++ controllers/authentication.go
@@ -1,1 +1,33 @@
 package controllers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/unrolled/render"
+
+	"github.com/briandowns/aion/config"
+	"github.com/briandowns/aion/database"
+)
+
+// Token holds the auth token
+type Token struct {
+	Raw       string                 // The raw token.  Populated when you Parse a token
+	Method    SigningMethod          // The signing method used or to be used
+	Header    map[string]interface{} // The first segment of the token
+	Claims    map[string]interface{} // The second segment of the token
+	Signature string                 // The third segment of the token.  Populated when you Parse a token
+	Valid     bool                   // Is the token valid?  Populated when you Parse/Verify a token
+}
+
+// LoginRouteHandler provides the handler for user logins
+func LoginRouteHandler(ren *render.Render, conf *config.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		db, err := database.NewDatabase(conf)
+		if err != nil {
+			log.Println(err)
+		}
+		defer db.Conn.Close()
+		ren.JSON(w, http.StatusOK, map[string]interface{}{"tasks": db.GetUsers()})
+	}
+}
