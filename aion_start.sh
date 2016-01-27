5269c6cef0706c73bfdc3d8802fdb79adcffdacc
Steven Hopkins <srhopkins@gmail.com>
2016-01-26 22:58:03 -0700

    Switching from aion to aiond command name

--- aion_start.sh
+++ aion_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RUN_AION='aion -db-user aion -db-pass aion -db-host db -db-port 3306 -db-name aion -nsq-host nsqd'
+RUN_AION='aiond -db-user aion -db-pass aion -db-host db -db-port 3306 -db-name aion -nsq-host nsqd'
 
 command -v docker-compose &>/dev/null \
     && RUN_AION="docker-compose run --service-ports aion ${RUN_AION}"
