63b84c6df775fe24038a90f81af62e8d4f3911c0
Steven Hopkins <srhopkins@gmail.com>
2016-01-27 09:53:09 -0700

    Updating to run in background and expose port.

--- aion_start.sh
+++ aion_start.sh
@@ -3,6 +3,6 @@
 RUN_AION='aiond -db-user aion -db-pass aion -db-host db -db-port 3306 -db-name aion -nsq-host nsqd'
 
 command -v docker-compose &>/dev/null \
-    && RUN_AION="docker-compose run --service-ports aion ${RUN_AION}"
+    && RUN_AION="docker-compose run -d --service-ports aion ${RUN_AION}"
 
 eval "${RUN_AION} $@"
